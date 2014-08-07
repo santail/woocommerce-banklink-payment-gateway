def1f413e2def91fd98984eadab5f18c56064cb9
nikolai muhhin <test@test.ee>
2014-08-07 14:39:58 +0000

    Code cleanup. Remove unneeded comments. Rearrange actions and remove unneeded methods.

--- index.php
+++ index.php
@@ -119,34 +119,24 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
                 $this->form_submission_method = $this->get_option('form_submission_method') == 'yes' ? true : false;
                 $this->title = $this->get_option('title');
                 $this->description = $this->get_option('description');
-                $this->redirect_page_id = $this->get_option('redirect_page_id');
 
                 // Logs
                 if ('yes' == $this->debug)
                     $this->log = $woocommerce->logger();
 
                 // Actions
-                add_action('valid-banklink-standard-ipn-request', array($this, 'successful_request'));
                 add_action('woocommerce_receipt_banklink', array($this, 'receipt_page'));
                 add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
 
-                add_action('woocommerce_checkout_process', array($this, 'my_custom_checkout_field_process'));
-                add_action('woocommerce_checkout_update_order_meta', array( &$this, 'combine_street_number_suffix' ) );
+                // Validate checkout form for payment method and selected method type if Banklink is chosen
+                add_action('woocommerce_checkout_process', array($this, 'checkout_process'));
 
-
-
-                // Payment listener/API hook
-                add_action('woocommerce_api_wc_gateway_banklink', array($this, 'check_ipn_response'));
+                // Add chosen payment method to order meta
+                add_action('woocommerce_checkout_update_order_meta', array($this, 'update_order_meta_with_custom_values'));
 
                 if (!$this->is_valid_for_use()) $this->enabled = false;
             }
 
-            /**
-             * Check if this gateway is enabled and available in the user's country
-             *
-             * @access public
-             * @return bool
-             */
             function is_valid_for_use()
             {
                 if (!in_array(get_woocommerce_currency(), apply_filters('woocommerce_paypal_supported_currencies', array('AUD', 'BRL', 'CAD', 'MXN', 'NZD', 'HKD', 'SGD', 'USD', 'EUR', 'JPY', 'TRY', 'NOK', 'CZK', 'DKK', 'HUF', 'ILS', 'MYR', 'PHP', 'PLN', 'SEK', 'CHF', 'TWD', 'THB', 'GBP', 'RMB')))) return false;
@@ -154,26 +144,21 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
                 return true;
             }
 
-            public function combine_street_number_suffix ( $order_id ) {
-                // check for suffix
-                echo $_POST['banklink_sel_bank'];
+            function checkout_process($posted)
+            {
+                if ($posted['payment_method'] == $this->id) {
+                    global $woocommerce;
 
-
-                return;
+                    // Check if set, if its not set add an error.
+                    if (!$_POST['banklink_sel_bank']) {
+                        $woocommerce->add_error(__('Please mark payment method'));
+                    }
+                }
             }
 
-            function my_custom_checkout_field_process() {
-                global $woocommerce;
-
-                echo 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb';
-
-                // Check if set, if its not set add an error.
-                if (!$_POST['banklink_sel_bank']) {
-                    $woocommerce->add_error( __('Please enter something into this new shiny field.') );
-                }
-                else {
-                    echo $_POST['banklink_sel_bank'];
-                }
+            function update_order_meta_with_custom_values($order_id)
+            {
+                if ($_POST['banklink_sel_bank']) update_post_meta($order_id, 'banklink_sel_bank', esc_attr($_POST['banklink_sel_bank']));
             }
 
             function init_form_fields()
@@ -312,192 +297,6 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
                 echo '</div>';
             }
 
-            /**
-             * Successful Payment!
-             *
-             * @access public
-             * @param array $posted
-             * @return void
-             */
-            function successful_request($posted)
-            {
-                global $woocommerce;
-
-                $posted = stripslashes_deep($posted);
-
-                // Custom holds post ID
-                if (!empty($posted['invoice']) && !empty($posted['custom'])) {
-
-                    $order = $this->get_paypal_order($posted);
-
-                    if ('yes' == $this->debug)
-                        $this->log->add('paypal', 'Found order #' . $order->id);
-
-                    // Lowercase returned variables
-                    $posted['payment_status'] = strtolower($posted['payment_status']);
-                    $posted['txn_type'] = strtolower($posted['txn_type']);
-
-                    // Sandbox fix
-                    if ($posted['test_ipn'] == 1 && $posted['payment_status'] == 'pending')
-                        $posted['payment_status'] = 'completed';
-
-                    if ('yes' == $this->debug)
-                        $this->log->add('paypal', 'Payment status: ' . $posted['payment_status']);
-
-                    // We are here so lets check status and do actions
-                    switch ($posted['payment_status']) {
-                        case 'completed' :
-                        case 'pending' :
-
-                            // Check order not already completed
-                            if ($order->status == 'completed') {
-                                if ('yes' == $this->debug)
-                                    $this->log->add('paypal', 'Aborting, Order #' . $order->id . ' is already complete.');
-                                exit;
-                            }
-
-                            // Check valid txn_type
-                            $accepted_types = array('cart', 'instant', 'express_checkout', 'web_accept', 'masspay', 'send_money');
-                            if (!in_array($posted['txn_type'], $accepted_types)) {
-                                if ('yes' == $this->debug)
-                                    $this->log->add('paypal', 'Aborting, Invalid type:' . $posted['txn_type']);
-                                exit;
-                            }
-
-                            // Validate Amount
-                            if ($order->get_total() != $posted['mc_gross']) {
-
-                                if ('yes' == $this->debug)
-                                    $this->log->add('paypal', 'Payment error: Amounts do not match (gross ' . $posted['mc_gross'] . ')');
-
-                                // Put this order on-hold for manual checking
-                                $order->update_status('on-hold', sprintf(__('Validation error: PayPal amounts do not match (gross %s).', 'woocommerce'), $posted['mc_gross']));
-
-                                exit;
-                            }
-
-                            // Validate Email Address
-                            if (strcasecmp(trim($posted['receiver_email']), trim($this->receiver_email)) != 0) {
-                                if ('yes' == $this->debug)
-                                    $this->log->add('paypal', "IPN Response is for another one: {$posted['receiver_email']} our email is {$this->receiver_email}");
-
-                                // Put this order on-hold for manual checking
-                                $order->update_status('on-hold', sprintf(__('Validation error: PayPal IPN response from a different email address (%s).', 'woocommerce'), $posted['receiver_email']));
-
-                                exit;
-                            }
-
-                            // Store PP Details
-                            if (!empty($posted['payer_email']))
-                                update_post_meta($order->id, 'Payer PayPal address', $posted['payer_email']);
-                            if (!empty($posted['txn_id']))
-                                update_post_meta($order->id, 'Transaction ID', $posted['txn_id']);
-                            if (!empty($posted['first_name']))
-                                update_post_meta($order->id, 'Payer first name', $posted['first_name']);
-                            if (!empty($posted['last_name']))
-                                update_post_meta($order->id, 'Payer last name', $posted['last_name']);
-                            if (!empty($posted['payment_type']))
-                                update_post_meta($order->id, 'Payment type', $posted['payment_type']);
-
-                            if ($posted['payment_status'] == 'completed') {
-                                $order->add_order_note(__('IPN payment completed', 'woocommerce'));
-                                $order->payment_complete();
-                            } else {
-                                $order->update_status('on-hold', sprintf(__('Payment pending: %s', 'woocommerce'), $posted['pending_reason']));
-                            }
-
-                            if ('yes' == $this->debug)
-                                $this->log->add('paypal', 'Payment complete.');
-
-                            break;
-                        case 'denied' :
-                        case 'expired' :
-                        case 'failed' :
-                        case 'voided' :
-                            // Order failed
-                            $order->update_status('failed', sprintf(__('Payment %s via IPN.', 'woocommerce'), strtolower($posted['payment_status'])));
-                            break;
-                        case "refunded" :
-
-                            // Only handle full refunds, not partial
-                            if ($order->get_total() == ($posted['mc_gross'] * -1)) {
-
-                                // Mark order as refunded
-                                $order->update_status('refunded', sprintf(__('Payment %s via IPN.', 'woocommerce'), strtolower($posted['payment_status'])));
-
-                                $mailer = $woocommerce->mailer();
-
-                                $message = $mailer->wrap_message(
-                                    __('Order refunded/reversed', 'woocommerce'),
-                                    sprintf(__('Order %s has been marked as refunded - PayPal reason code: %s', 'woocommerce'), $order->get_order_number(), $posted['reason_code'])
-                                );
-
-                                $mailer->send(get_option('admin_email'), sprintf(__('Payment for order %s refunded/reversed', 'woocommerce'), $order->get_order_number()), $message);
-
-                            }
-
-                            break;
-                        case "reversed" :
-
-                            // Mark order as refunded
-                            $order->update_status('on-hold', sprintf(__('Payment %s via IPN.', 'woocommerce'), strtolower($posted['payment_status'])));
-
-                            $mailer = $woocommerce->mailer();
-
-                            $message = $mailer->wrap_message(
-                                __('Order reversed', 'woocommerce'),
-                                sprintf(__('Order %s has been marked on-hold due to a reversal - PayPal reason code: %s', 'woocommerce'), $order->get_order_number(), $posted['reason_code'])
-                            );
-
-                            $mailer->send(get_option('admin_email'), sprintf(__('Payment for order %s reversed', 'woocommerce'), $order->get_order_number()), $message);
-
-                            break;
-                        case "canceled_reversal" :
-
-                            $mailer = $woocommerce->mailer();
-
-                            $message = $mailer->wrap_message(
-                                __('Reversal Cancelled', 'woocommerce'),
-                                sprintf(__('Order %s has had a reversal cancelled. Please check the status of payment and update the order status accordingly.', 'woocommerce'), $order->get_order_number())
-                            );
-
-                            $mailer->send(get_option('admin_email'), sprintf(__('Reversal cancelled for order %s', 'woocommerce'), $order->get_order_number()), $message);
-
-                            break;
-                        default :
-                            // No action
-                            break;
-                    }
-
-                    exit;
-                }
-
-            }
-
-            /**
-             * Check for PayPal IPN Response
-             *
-             * @access public
-             * @return void
-             */
-            function check_ipn_response()
-            {
-                @ob_clean();
-
-                if (!empty($_POST) && $this->check_ipn_request_is_valid()) {
-
-                    header('HTTP/1.1 200 OK');
-
-                    do_action("valid-banklink-standard-ipn-request", $_POST);
-
-                } else {
-
-                    wp_die("PayPal IPN Request Failure");
-
-                }
-
-            }
-
             function payment_fields()
             {
                 echo '<table class="crt_3" id="bank_links"> <tbody><tr> <td class="p10" align="center"> <nobr> ';
@@ -586,90 +385,14 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
                 </form>';
             }
 
-            function process_payment($order_id) {
+            function process_payment($order_id) 
+            {
                 $order = new WC_Order($order_id);
 
                 return array(
                     'result' 	=> 'success',
                     'redirect'	=> add_query_arg('order', $order->id, add_query_arg('key', $order->order_key, get_permalink(woocommerce_get_page_id('pay'))))
                 );
-            }
-
-            /**
-             * Check for valid payu server callback
-             **/
-            function check_payu_response()
-            {
-                global $woocommerce;
-                if (isset($_REQUEST['txnid']) && isset($_REQUEST['mihpayid'])) {
-                    $order_id_time = $_REQUEST['txnid'];
-                    $order_id = explode('_', $_REQUEST['txnid']);
-                    $order_id = (int)$order_id[0];
-                    if ($order_id != '') {
-                        try {
-                            $order = new WC_Order($order_id);
-                            $merchant_id = $_REQUEST['key'];
-                            $amount = $_REQUEST['Amount'];
-                            $hash = $_REQUEST['hash'];
-
-                            $status = $_REQUEST['status'];
-                            $productinfo = "Order $order_id";
-                            echo $hash;
-                            echo "{$this->salt}|$status|||||||||||{$order->billing_email}|{$order->billing_first_name}|$productinfo|{$order->order_total}|$order_id_time|{$this->merchant_id}";
-                            $checkhash = hash('sha512', "{$this->salt}|$status|||||||||||{$order->billing_email}|{$order->billing_first_name}|$productinfo|{$order->order_total}|$order_id_time|{$this->merchant_id}");
-                            $transauthorised = false;
-                            if ($order->status !== 'completed') {
-                                if ($hash == $checkhash) {
-
-                                    $status = strtolower($status);
-
-                                    if ($status == "success") {
-                                        $transauthorised = true;
-                                        $this->msg['message'] = "Thank you for shopping with us. Your account has been charged and your transaction is successful. We will be shipping your order to you soon.";
-                                        $this->msg['class'] = 'woocommerce_message';
-                                        if ($order->status == 'processing') {
-
-                                        } else {
-                                            $order->payment_complete();
-                                            $order->add_order_note('PayU payment successful<br/>Unnique Id from PayU: ' . $_REQUEST['mihpayid']);
-                                            $order->add_order_note($this->msg['message']);
-                                            $woocommerce->cart->empty_cart();
-                                        }
-                                    } else if ($status == "pending") {
-                                        $this->msg['message'] = "Thank you for shopping with us. Right now your payment staus is pending, We will keep you posted regarding the status of your order through e-mail";
-                                        $this->msg['class'] = 'woocommerce_message woocommerce_message_info';
-                                        $order->add_order_note('PayU payment status is pending<br/>Unnique Id from PayU: ' . $_REQUEST['mihpayid']);
-                                        $order->add_order_note($this->msg['message']);
-                                        $order->update_status('on-hold');
-                                        $woocommerce->cart->empty_cart();
-                                    } else {
-                                        $this->msg['class'] = 'woocommerce_error';
-                                        $this->msg['message'] = "Thank you for shopping with us. However, the transaction has been declined.";
-                                        $order->add_order_note('Transaction Declined: ' . $_REQUEST['Error']);
-                                        //Here you need to put in the routines for a failed
-                                        //transaction such as sending an email to customer
-                                        //setting database status etc etc
-                                    }
-                                } else {
-                                    $this->msg['class'] = 'error';
-                                    $this->msg['message'] = "Security Error. Illegal access detected";
-
-                                    //Here you need to simply ignore this and dont need
-                                    //to perform any operation in this condition
-                                }
-                                if ($transauthorised == false) {
-                                    $order->update_status('failed');
-                                    $order->add_order_note('Failed');
-                                    $order->add_order_note($this->msg['message']);
-                                }
-                                add_action('the_content', array(&$this, 'showMessage'));
-                            }
-                        } catch (Exception $e) {
-                            // $errorOccurred = true;
-                            $msg = "Error";
-                        }
-                    }
-                }
             }
 
             function showMessage($content)
